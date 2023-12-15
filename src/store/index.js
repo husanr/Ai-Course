cc5ffeb56b62bd0a47e9823aeafd64a7fb5ed183
husanr <1090037611@qq.com>
2023-12-15 10:32:33 +0800

    feat: 新增内容文本变量

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import { reactive } from 'vue'
 export const store = reactive({
   activeIndex: 0,
   outlineText: "第一章：课题1\n\t第一节：节目1\n\t第二节：节目2\n\t第三节：节目3\n\n第二章：课题2\n\t第一节：节目1\n\t第二节：节目2\n\t第三节：节目3",
+  contentText: "第一章：内容1\n\t第一节：节目1\n\t第二节：节目2\n\t第三节：节目3\n\n第二章：内容2\n\t第一节：节目1\n\t第二节：节目2\n\t第三节：节目3",
   lectureText: "讲稿1\n\t" +
                   "第一节：节目1\n\t" +
                   "第二节：节目2\n\t" +
